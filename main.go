3561323e104c7b0fa45c4f11143093b4e690b601
kohei.yaginuma <f.kohei.yaginuma@gmail.com>
2022-12-09 09:30:07 +0900

    add `GET /healthz`

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo"
 	"github.com/yagi_eng/group_shuffle_gui/handler"
 )
@@ -13,6 +15,10 @@ func main() {
 }
 
 func initRouting(e *echo.Echo) {
+	e.GET("/healthz", func(c echo.Context) error {
+		return c.NoContent(http.StatusOK)
+	})
+
 	e.File("/", "web/index.html")
 	// http://localhost:1323/api/v1/simulateCombinations?allParticipants=18&participantsInEachGroup=6&repeatCnt=3&trials=10000
 	e.GET("/api/v1/simulateCombinations", handler.SimulateCombinations)
